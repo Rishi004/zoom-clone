68388964e0b1242affb3bc24217f86591ee46cc5
Rishi004 <rishireiko@gmail.com>
2024-04-12 12:13:54 +0530

    set up new meeting room

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from "@/providers/StreamClienProvider";
-import React, { ReactNode } from "react";
+import { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+    title: "BOOM",
+    description: "Video calling app",
+    icons: {
+        icon: "/icons/logo.svg"
+    }
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
