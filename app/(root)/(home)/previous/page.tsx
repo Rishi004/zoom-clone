ffa1263003457cfe8a6b05f02d98f6207c80bc04
Rishi004 <rishireiko@gmail.com>
2024-04-23 22:15:30 +0530

    previous meeting page

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import CallList from "@/components/CallList";
 
 const Previous = () => {
     return (
         <section className="flex size-full flex-col gap-10 text-white">
             <h1 className="text-3xl font-bold">Previous</h1>
+            <CallList type="ended" />
         </section>
     );
 };
